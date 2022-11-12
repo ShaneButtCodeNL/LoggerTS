9851c4c1f04bbb2d84d25d57031279360b14b836
shane butt <shanebuttcode@gmail.com>
2022-11-12 14:35:16 -0330

    added Config for chalk colors

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+export default {
+  levels: {
+    info: {
+      color: "#3AFF00", //Bright Green
+      writeToFile: true,
+    },
+    warn: {
+      color: "#ff960a", //Orange
+      writeToFile: true,
+    },
+    debug: {
+      color: "#229500", //Dark Green
+      writeToFile: true,
+    },
+    system: {
+      color: "#ffffff", //white
+      writeToFile: true,
+    },
+    error: {
+      color: "#db0000", //Red
+      writeToFile: true,
+    },
+    fatal: {
+      color: "#ff0000", //Redder
+      writeToFile: true,
+    },
+    database: {
+      color: "#0024FF", //Dark Blue
+      writeToFile: true,
+    },
+    event: {
+      color: "#00FFDB", //Cyan
+      writeToFile: true,
+    },
+  },
+};
